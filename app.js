6928f56c451ff82b2dcd493157dfd7880cdccb5a
github username <github email address>
2019-10-06 11:39:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function list(names){
+    let nameList = [];
+    for(var i = 0; i < names.length; i++) {
+      var newNames = names[i].name;
+      console.log(names[i].name);
+        // 
+        // for(var j=0; j < newNames[i].length; j++){
+        //   console.log(newNames.join)
+        // }
+        
+    }
+    
+      //your code here
+    }
+    
+    list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}, {name: 'Jessie'}])
+    
+    //first list out all the names
+    //then find the last 2 names
+    //then combine them with &
+    //then put a comma between all the other names
+    //join the names in an array
+    //return the array
